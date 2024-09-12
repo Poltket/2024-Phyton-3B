5049400c228acd922d4b13e2e4cbf8c4b9ac1ff5
Ketlyn Poltronhere <106679371+Poltket@users.noreply.github.com>
2024-09-12 10:31:49 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,26 +1,44 @@
-print("""
-           
-██████╗░██╗░░██╗░█████╗░██████╗░░██████╗
-██╔══██╗██║░██╔╝██╔══██╗██╔══██╗██╔════╝
-██████╔╝█████═╝░███████║██████╔╝╚█████╗░
-██╔══██╗██╔═██╗░██╔══██║██╔══██╗░╚═══██╗
-██║░░██║██║░╚██╗██║░░██║██║░░██║██████╔╝
-╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+import os
 
-      """)
-print('1. Cadastro de carros')
-print('2. Listar carros')
-print('3. Ativar carros')
-print('4. Sair da aplicação')
+def mostra_titulo():
+    print("""
+            
+    ██████╗░██╗░░██╗░█████╗░██████╗░░██████╗
+    ██╔══██╗██║░██╔╝██╔══██╗██╔══██╗██╔════╝
+    ██████╔╝█████═╝░███████║██████╔╝╚█████╗░
+    ██╔══██╗██╔═██╗░██╔══██║██╔══██╗░╚═══██╗
+    ██║░░██║██║░╚██╗██║░░██║██║░░██║██████╔╝
+    ╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
 
-opcao_escolhida = int(input('Escolha uma opção: '))
-print('Você escolheu a opção: ', opcao_escolhida)
+        """)
+    
+def mostra_escolha():
+    print('1. Cadastro de carros')
+    print('2. Listar carros')
+    print('3. Ativar carros')
+    print('4. Sair da aplicação')
 
-if opcao_escolhida == 1:
-    print('Cadastrar carro')
-elif opcao_escolhida == 2:
-    print('Listar carro')
-elif opcao_escolhida == 3:
-    print('Ativar/desativar carro')
-else:
-    print('Saindo da aplicação')
+def escolhe_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    print('Você escolheu a opção: ', opcao_escolhida)
+
+    def finalizar_programa():
+        os. syatem('cls')
+        print('Finalizando programa')
+
+    if opcao_escolhida == 1:
+        print('Cadastrar carro')
+    elif opcao_escolhida == 2:
+        print('Listar carro')
+    elif opcao_escolhida == 3:
+        print('Ativar/desativar carro')
+    else:
+        finalizar_programa()
+
+def main():
+    mostra_titulo()
+    mostra_escolha()
+    escolhe_opcao()
+
+if __name__ == '__main__':
+    main()
