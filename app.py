068f198d7238ba7c5215d512efef56fbd72bc47c
Ketlyn Poltronhere <106679371+Poltket@users.noreply.github.com>
2024-09-19 10:27:07 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 import os
 
+carros = []
+
+def exibir_subtitulo(texto):
+    os.system('cls')
+    print(texto)
+    print('')
+
+def retorna_menu_principal():
+     input('\n Digite qualquer tecla para voltar ao menu principal')
+     main()
+
 def mostra_titulo():
     print("""
             
@@ -19,21 +30,50 @@ def mostra_escolha():
     print('4. Sair da aplicação')
 
 def escolhe_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    print('Você escolheu a opção: ', opcao_escolhida)
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        print('Você escolheu a opção: ', opcao_escolhida)
 
-    def finalizar_programa():
+        if opcao_escolhida == 1:
+            cadastrar_carros()
+        elif opcao_escolhida == 2:
+            mostrar_carros
+        elif opcao_escolhida == 3:
+            print('Ativar/desativar carro')
+        elif opcao_escolhida == 4:
+            finalizar_programa()
+        else:
+            opcao_invalida()
+
+    except:
+         opcao_invalida()
+
+def cadastrar_carros():
+    exibir_subtitulo('Cadastrar carros')
+
+    nome_carro = input('Digite o nome do carro: ')
+    carros.append(nome_carro)
+    print(f'{nome_carro} foi adicionado a lista de carros')
+    input('Digite qualquer tecla para voltar')
+    main()
+
+def mostrar_carros():
+    exibir_subtitulo('Listar carros')
+    
+    for carro in carros:
+         print(f' - carro')
+
+    input('Digite qualquer tecla para voltar')
+    main()
+
+def finalizar_programa():
         os. syatem('cls')
         print('Finalizando programa')
 
-    if opcao_escolhida == 1:
-        print('Cadastrar carro')
-    elif opcao_escolhida == 2:
-        print('Listar carro')
-    elif opcao_escolhida == 3:
-        print('Ativar/desativar carro')
-    else:
-        finalizar_programa()
+def opcao_invalida():
+        print('Este caracter não é permitido')
+        input('Digite qualquer tecla')
+        main()
 
 def main():
     mostra_titulo()
